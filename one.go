ea979513dbf0510c02e6d5bd645566e25bc836b0
Pratham Aditya Salhotra <prathamadityasalhotra@gmail.com>
2022-10-03 19:41:14 +0530

    first go code

---
+++ one.go
@@ -0,0 +1,12 @@
+// you have to decelare that what package you want to use
+// main is the special package this is going to be the entry point of the app
+
+package main
+
+// import additional packages and this one for strings
+import "fmt"
+
+// entry point of the app
+func main() {
+    fmt.Println("Hey devoops!")
+} 
